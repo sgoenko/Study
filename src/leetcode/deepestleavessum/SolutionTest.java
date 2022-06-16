0a302bfda7d93c5d7e36e6e095ddf04b6f5b8356
sgoenko <sgoenko@gmail.com>
2022-06-16 22:34:55 +0300

    deepestLeavesSum

--- src/leetcode/deepestleavessum/SolutionTest.java
+++ src/leetcode/deepestleavessum/SolutionTest.java
@@ -11,10 +11,17 @@ public class SolutionTest {
 	@Test
 	public void deepestLeavesSumTest() {
 		TreeNode root = new TreeNode(1);
-		root.left = new TreeNode(2);
-		root.right = new TreeNode(3);
 
-		assertEquals(5, solution.deepestLeavesSum(root));
+		TreeNode left = new TreeNode(2);
+		left.left = new TreeNode(4);
+		left.right = new TreeNode(5);
+		root.left = left;
+
+		TreeNode right = new TreeNode(3);
+		right.right = new TreeNode(6);
+		root.right = right;
+
+		assertEquals(15, solution.deepestLeavesSum(root));
 	}
 
 }
